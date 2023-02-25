8b0e8438c3f5b120f8e315c8c9956a37805ff9e8
Yongsoo Cho <97702731+Yongsoo-Cho@users.noreply.github.com>
2023-02-25 12:48:58 -0500

    Create Register.js

---
+++ frontend/src/register/Register.js
@@ -0,0 +1,7 @@
+// REGISTER LOGIC GOES HERE.
+// Notes:
+//   - Rename Register.js based on naming conventions after it is decided on (i.e. capitalizations for styles, main, and js files)
+//   - Don't put everything in one file. Seperate into:
+//       - Styles
+//       - Logic (Main .js file)
+//       - APIs (Accessory .js files)
