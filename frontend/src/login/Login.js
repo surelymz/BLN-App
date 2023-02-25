4627f56817ab661858405ec628ab4800cf938136
Yongsoo Cho <97702731+Yongsoo-Cho@users.noreply.github.com>
2023-02-25 12:48:08 -0500

    Create folder for login

---
+++ frontend/src/login/Login.js
@@ -0,0 +1,7 @@
+// LOGIN LOGIC GOES HERE.
+// Notes:
+//   - Rename Login.js based on naming conventions after it is decided on (i.e. capitalizations for styles, main, and js files)
+//   - Don't put everything in one file. Seperate into:
+//       - Styles
+//       - Logic (Main .js file)
+//       - APIs (Accessory .js files)
