e9ddb00812ea2cd565414ca57efd9b91a8181982
Yongsoo Cho <97702731+Yongsoo-Cho@users.noreply.github.com>
2023-02-25 12:52:39 -0500

    Create Courses.js

---
+++ frontend/src/courses/Courses.js
@@ -0,0 +1,7 @@
+// COURSES HOME PAGE LOGIC GOES HERE.
+// Notes:
+//   - Rename Courses.js based on naming conventions after it is decided on (i.e. capitalizations for styles, main, and js files)
+//   - Don't put everything in one file. Seperate into:
+//       - Styles
+//       - Logic (Main .js file)
+//       - APIs (Accessory .js files)
